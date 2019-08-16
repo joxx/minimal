0f77e696f5091c64b04a978d0ca7db31f6658709
Joachim Kliemann <joachim.kliemann@googlemail.com>
2019-08-16 13:32:53 +0200

    add simple js color rotator

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 import '../css/style.scss'
 
 console.log('hello, welcome to monkey mountain')
+
+var colors = ["red", "orange", "green", "blue", "hotpink", "purple"];
+var currentIndex = 0;
+
+setInterval(function() {
+  document.querySelectorAll('.fa-monkey')[0].style.cssText = "color: " + colors[currentIndex];
+	currentIndex++;
+	if (currentIndex == undefined || currentIndex >= colors.length) {
+		currentIndex = 0;
+	}
+}, 1000);
